1cd70c9f665eaa4539442764df04b85b5bf3fc16
unknown <Dingkzhang@utexas.edu>
2024-05-10 11:14:12 -0500

    adding pdf converter to create jpg and txt asset for rules

--- pdf_converter.py
+++ pdf_converter.py
@@ -3,10 +3,10 @@
 # from tempfile import TemporaryDirectory
 # from pathlib import Path
  
-# import pytesseract
+import pytesseract
 # from pdf2image import convert_from_path
-# from PIL import Image
- 
+from PIL import Image
+import os
 
 # print(platform.system())
 
@@ -24,12 +24,39 @@
 
 # import module
 from pdf2image import convert_from_path
+parent_dir = os.environ.get('BOARDGAME_ASSET_PATH')
+
  
- 
+def create_folder(bg_name='railroad_ink'):
+
+  directory = bg_name
+  path = os.path.join(parent_dir, directory)
+
+  try: 
+      os.makedirs(path, exist_ok = True) 
+      print("Directory '%s' created successfully" % directory) 
+  except OSError as error: 
+      print("Directory '%s' can not be created" % directory)
+
 # Store Pdf with convert_from_path function
-images = convert_from_path('railroad_ink_rules.pdf')
- 
-for i in range(len(images)):
-   
-      # Save pages as images in the pdf
-    images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+def convert_PDF_to_JPEG(bg_name='railroad_ink'):
+  images = convert_from_path(parent_dir + '/' + bg_name +  '_rules.pdf')
+  
+  for i in range(len(images)):
+        # Save pages as images in the pdf
+      images[i].save(parent_dir + '/' + bg_name + '/' + bg_name + '_page'+ str(i) +'.jpg', 'JPEG')
+
+def convert_JPEG_to_TxT(bg_name='railroad_ink'):
+  pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+  images = convert_from_path(parent_dir + '/' + bg_name +  '_rules.pdf')
+
+  for i in range(len(images)):
+    rules_text = pytesseract.image_to_string(Image.open(parent_dir + '/' + bg_name + '/' + bg_name + '_page'+ str(i) +'.jpg')).encode()
+    with open(parent_dir + '/' + bg_name + '/' + bg_name + '_page'+ str(i) +'.txt', 'wb') as f:
+       f.write(rules_text)
+
+
+# create_folder()
+# convert_PDF_to_JPEG()
+convert_JPEG_to_TxT()
