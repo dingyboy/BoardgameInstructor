ad31596f199c3de3fdd513432d319f22f73afc68
Dingkun Zhang <dingkzhang@gmail.com>
2024-05-16 17:16:03 -0500

    adding mascot and initial page

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import streamlit as st
 import os
-st.title('Greatest of Three Numbers') # Set Title of the webapp
 
-choice1 = st.number_input('Enter First number') #Accepts a number input
-choice2 = st.number_input('Enter Second number')
-choice3 = st.number_input('Enter Third number')
 
-string = f'Maximum value is {max(choice1,choice2,choice3)}'
+col1, col2 = st.columns(2)
 
-#test
-st.write(string)
+with col1:
+    st.title('Boardgame Buddy') # Set Title of the webapp
+with col2:
+    st.image('boardgame_buddy.png')
+
+# choice1 = st.number_input('Enter First number') #Accepts a number input
+# choice2 = st.number_input('Enter Second number')
+# choice3 = st.number_input('Enter Third number')
+if 'entrance_greeting' not in st.session_state:
+    st.session_state['entrance_greeting'] = 'Slider to enter.'
+
+slider_value = st.slider("", 0, 100, 0)
+st.write(st.session_state.entrance_greeting)
+if (slider_value > 25):
+    st.session_state.entrance_greeting = "Wheeee!"
+if (slider_value > 50):
+    st.session_state.entrance_greeting = 'Whooaa! :o'
+if (slider_value > 75):
+    st.session_state.entrance_greeting = 'Ahhhhh! :D'
+if(slider_value > 95):
+    st.session_state.entrance_greeting = 'Here we go! :3'
 
 # print(os.environ.get('OPENAI_API_KEY'))
 # from openai import OpenAI
