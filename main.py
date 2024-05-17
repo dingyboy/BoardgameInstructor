1a0c8f353a9153d1cc21d27f81ab5751b86b473e
Dingkun Zhang <dingkzhang@gmail.com>
2024-05-17 14:06:17 -0500

    adding public assets and initial set

--- main.py
+++ main.py
@@ -3,23 +3,46 @@ import os
 import numpy as np
 from openai import OpenAI
 from dotenv import load_dotenv
-
+import json
 load_dotenv()
+
+
+BOARDGAME_LIST = []
+BOARDGAME_DISPLAY_LIST = []
+MODEL_LIST = []
+SYSTEM_PROMPT = ""
+
+with open('./public_asset/system_prompt.json') as a:
+    SYSTEM_PROMPT = json.load(a)['content']
+
+with open('./public_asset/boardgame_list.json') as b:
+    BOARDGAME_LIST = json.load(b)
+    for item in BOARDGAME_LIST:
+        print(item)
+        BOARDGAME_DISPLAY_LIST.append(item['display_name'])
+
+with open('./public_asset/model_list.json') as c:
+    model_temp_list = json.load(c)
+    for item in model_temp_list:
+        MODEL_LIST.append(item['name'])
+
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 client_openai = OpenAI(api_key=OPENAI_API_KEY)
 
 
 boardgame_list = ('Railroad Ink', 'Cascadia')
+model_list =('gpt-3.5-turbo', 'gpt-4o')
+
 
 col1, col2 = st.columns(2)
 with col1:
-    st.title('Boardgame Buddy') # Set Title of the webapp
+    st.title('Board Game Buddy') # Set Title of the webapp
 with col2:
-    st.image('boardgame_buddy.png')
+    st.image('./public_asset/boardgame_buddy.png')
 
-option = st.selectbox('Which pesky boardgame do you need help with?',
-        boardgame_list)
+boardgame_option = st.selectbox('Which pesky boardgame do you need help with?', BOARDGAME_DISPLAY_LIST)
+model_option = st.selectbox('Which model would you like to use?', MODEL_LIST)
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -33,11 +56,14 @@ if prompt := st.chat_input("What can I help you with?"):
         st.markdown(prompt)
     st.session_state.messages.append({"role":"user", "content":prompt})
 
+    # need to fetch from MongoDB Database here
+    # based on the promtp here: prompt
+    # maybe we have a checkbox to ask the user if we want to use images or not
 
     openai_response = client_openai.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model=model_option,
         messages= [
-            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "system", "content": SYSTEM_PROMPT},
             {"role": "user", "content": prompt}
         ]
     )
